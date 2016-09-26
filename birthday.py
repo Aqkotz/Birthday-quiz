7ee7c26bf0d8466b9f231da4eea8d30a191d82e6
Aqkotz <andy.lyme@mac.com>
2016-09-26 13:52:11 -0400

    Updated from Brython Server: 9/26/2016 1:52:11 PM

--- birthday.py
+++ birthday.py
@@ -60,7 +60,8 @@ else:
 
     else:
         #print ("Hi " + name + ", your birthday is " + birthmonth + " " + str(birthday) + ", " + str(birthyear) +".")
-    
+        if birthday == 31 and birthmonth in ["october", "October"]:
+            print ("your birthday is halloween!")
         if birthmonth in ["january", "January", "february", "February", "december", "December"]:
             birthseason = ("winter")
         if birthmonth in ["march", "March", "april", "April", "may", "May"]:
@@ -83,5 +84,3 @@ else:
             print ("you are a " + birthseason + " baby of the seventies")
         if birthyear < 1969:
             print ("you are a " + birthseason + " baby of the stone age")
-        if birthday == 31 and birthmonth in ["october", "October"]:
-            print ("your birthday is halloween!")
