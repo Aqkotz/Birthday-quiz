52f9717fbdea73e7600078880c63680ca6ab5398
Aqkotz <andy.lyme@mac.com>
2016-09-26 13:22:46 -0400

    Updated from Brython Server: 9/26/2016 1:22:46 PM

--- birthday.py
+++ birthday.py
@@ -35,13 +35,14 @@ name = input("Hello, what is your name? ")
 birthmonth = input("Hi " + name + ", what was the name of the month you were born in? ")
 birthyear = input("And what year were you born in, " + name + "? ")
 birthday = input("And the day? ")
-#leapyear = input("Is it a leapyear? ")
+if birthmonth in ["february", "February"]:
+    leapyear = input("Is it a leapyear? ")
 
 birthyear = int(birthyear)
 birthday = int(birthday)
-#print ("")
-#if leapyear not in ["Yes", "yes", "no", "No"]:
-    #print ("Not a valid response for 'is it a leapyear.'")
+print ("")
+if leapyear not in ["Yes", "yes", "no", "No"]:
+    print ("Not a valid response for 'is it a leapyear.'")
 else:
     if birthmonth not in ["january", "January", "february", "February", "march", "March", "april", "April", "may", "May", "june", "June", "july", "July", "august", "August", "september", "September", "october", "October", "november", "November", "december", "December", "Janember"]:
         print ("Sorry, " + birthmonth + " is not a real month.")
@@ -75,14 +76,16 @@ else:
             print ("you were a " + birthseason + " baby of the eighties")
         if birthyear < 1999 and birthyear > 1990:
             print ("you were a " + birthseason + " baby of the nineties")
-        if birthyear < 2009 and birthyear > 2000:
+        if birthyear < 2013 and birthyear > 2000:
             print ("you were a " + birthseason + " baby of the two thousands")
         if birthyear < 2016 and birthyear > 2014:
-            print ("you are a " + birthseason + " baby, and also an infant")
+            print ("you were a " + birthseason + " baby, and also an infant")
         if birthyear < 1979 and birthyear > 1970:
             print ("you were a " + birthseason + " baby of the seventies")
         if birthyear < 1969 and birthyear > 1960:
             print ("you were a " + birthseason + " baby of the sixties")
         if birthyear < 1959 and birthyear > 1950:
             print ("you were a " + birthseason + " baby of the nineties")
+        if birthday == 31 and birthmonth in ["october", "October"]:
+            print ("your birthday is halloween!")
 
